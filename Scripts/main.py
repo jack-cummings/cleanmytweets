5d0ca72c4d1f8ea90dea3260d229e4d4f96cfa76
jack-cummings <jmcummi2@ncsu.edu>
2021-12-28 17:47:16 -0500

    convert to tweepy

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,5 +1,6 @@
 import pytwitter
 from pytwitter import Api
+import tweepy
 import getpass
 import pandas as pd
 import re
@@ -9,39 +10,45 @@ import os
 from flask import Flask, render_template_string, redirect, request
 
 # Load Webpage strings
-homePage = '''
-<!doctype html>
-<html>
-    <head>
-        <!-- Bootstrap CSS --> 
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    </head>
-    <body>
-        <div class="container" style="padding:10px">
-                <div class="card mb-3 h-100">
-                    <div class="card-header">Welcome to socAIl! Please Enter your User ID Below </div>     
-                    <div class="card-body"> 
-                        <form method="POST" action="setUser"> 
-                            <div class="form-group" role="group" > 
-                                <label for="setUser">Please Enter Your User ID Here</label>
-                                <input type="text" class="form-control" id="setUser" placeholder="@ESPN" name=user>
-                            </div>
-                                <button type="submit" class="btn btn-primary">Submit</button>
-                            </div>
-                        </form> 
-                    </div>    
-                </div>
-        </div>
-    </body>
-</html>    '''
-returnPage = '''
-<!doctype html>
-<html>
-   <body>
-     <strong>{} Profane Tweets Found</strong>
-   </body>
-</html>
-'''
+# homePage = '''
+# <!doctype html>
+# <html>
+#     <head>
+#         <!-- Bootstrap CSS -->
+#         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+#     </head>
+#     <body>
+#         <div class="container" style="padding:10px">
+#                 <div class="card mb-3 h-100">
+#                     <div class="card-header">Welcome to socAIl! Please Enter your User ID Below </div>
+#                     <div class="card-body">
+#                         <form method="POST" action="setUser">
+#                             <div class="form-group" role="group" >
+#                                 <label for="setUser">Please Enter Your User ID Here</label>
+#                                 <input type="text" class="form-control" id="setUser" placeholder="@ESPN" name=user>
+#                             </div>
+#                                 <button type="submit" class="btn btn-primary">Submit</button>
+#                             </div>
+#                         </form>
+#                     </div>
+#                 </div>
+#         </div>
+#     </body>
+# </html>    '''
+# returnPage = '''
+# <!doctype html>
+# <html>
+#    <body>
+#      <strong>{} Profane Tweets Found</strong>
+#      <strong>{text}</strong>
+#    </body>
+# </html>
+# '''
+
+def HtmlIntake(path):
+    with open(path) as f:
+        lines = f.readlines()
+    return ''.join(lines)
 
 def loadWords():
     f = open("../Docs/profane_words.json", 'r')
@@ -50,10 +57,19 @@ def loadWords():
 
 
 def initTwitter():
-    bearer_token = os.environ['BEARER_TOKEN']
-    api = Api(bearer_token)
+    # bearer_token = os.environ['BEARER_TOKEN']
+    # api = Api(bearer_token)
+    # assign the values accordingly
+    auth = tweepy.AppAuthHandler(os.environ['api_key'], os.environ['api_secret'])
+    api = tweepy.API(auth)
     return api
 
+def scrape_tl_username(name):
+    temp_list =[]
+    for status in tweepy.Cursor(api.user_timeline, screen_name=name, tweet_mode="extended").items():
+        temp_list.append((status.full_text, status.created_at))
+    timeline_df = pd.DataFrame(temp_list, columns=['text','date'])
+    return timeline_df
 
 def scrape_tl(uid, count):
     """Function to collect user's timeline. UID is user ID number count is number of tweets to check. Max 32k"""
@@ -90,7 +106,7 @@ def flagDFProces(df):
 
 #  Per request
 def main(user_id):
-    df = scrape_tl(user_id, 100)
+    df = scrape_tl_username(user_id)
     print(f"Timeline Scrape Complete {df.shape[0]} tweet's collected")
     processed_df = flagDFProces(df)
     profane_df = processed_df[processed_df.occurance == 1]
@@ -107,8 +123,9 @@ def initWebsite(homePage, returnPage):
     @app.route("/setUser", methods=["POST"])
     def setUser():
         user = request.form["user"]
-        p_count = str(main(user).shape[0])
-        return render_template_string(returnPage.replace('{}', p_count))
+        temp_df = main(user)
+        p_count = str(temp_df.shape[0])
+        return render_template_string(returnPage.replace('{}', p_count).replace('{text}',str(temp_df.text.values)))
 
     app.run(debug=False)
 
@@ -116,6 +133,8 @@ def initWebsite(homePage, returnPage):
 #  initialization
 bad_words = loadWords()
 api = initTwitter()
+homePage = HtmlIntake("../Scripts/html/homepage.html")
+returnPage = HtmlIntake("../Scripts/html/returnPage.html")
 initWebsite(homePage,returnPage)
 print("Initialization Complete http://127.0.0.1:5000/ ")
 
