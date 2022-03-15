4fdc84abd0e5fe0e155c30cb646dd745358755a2
jack-cummings <jmcummi2@ncsu.edu>
2022-03-15 17:09:54 -0400

    begin stripe integration

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import re
 import json
 import os
 import datetime
+import stripe
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from starlette.responses import RedirectResponse
 
 if os.environ['MODE'] == 'dev':
     import uvicorn
@@ -80,31 +82,70 @@ async def results(request: Request):
     user = client.get_me(user_auth=False)
     username = user.data.username
     user_id = user.data.id
-
-    tweets_out = []
-    for tweet in tweepy.Paginator(client.get_users_tweets, id=user_id,
-                                  tweet_fields=['id', 'text', 'created_at'], max_results=100).flatten(limit=200):
-        tweets_out.append([tweet.id, tweet.text, tweet.created_at])
-
-    timeline_df = pd.DataFrame(tweets_out, columns=['Delete?', 'Text', 'date_full'])
-    tweet_count = timeline_df.shape[0]
-    app.df = timeline_df
+    app.user_id = user_id
     app.user = username
     app.client = client
 
-    return templates.TemplateResponse('tweet_count_j.html', {"request": request, "tweet_count": str(tweet_count)})
+    return templates.TemplateResponse('account_val.html', {"request": request, "user": username})
+
+
+# Fetch the Checkout Session to display the JSON result on the success page
+@app.get('/checkout-session', response_model=stripe.checkout.Session)
+def get_checkout_session(
+        sessionId: str
+):
+    id = sessionId
+    checkout_session = stripe.checkout.Session.retrieve(id)
+    return checkout_session
+
+
+@app.post('/create-checkout-session')
+def create_checkout_session():
+    domain_url = os.getenv('DOMAIN')
+    try:
+        # Create new Checkout Session for the order
+        # Other optional params include:
+
+        # For full details see https:#stripe.com/docs/api/checkout/sessions/create
+        # ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
+        checkout_session = stripe.checkout.Session.create(
+            success_url=domain_url + '/static/success.html?session_id={CHECKOUT_SESSION_ID}',
+            cancel_url=domain_url + '/static/canceled.html',
+            payment_method_types=(os.getenv('PAYMENT_METHOD_TYPES') or 'card').split(','),
+            mode='payment',
+            line_items=[{
+                'price': os.getenv('PRICE'),
+                'quantity': 1,
+            }]
+        )
+        return RedirectResponse(
+            checkout_session.url,
+            status.HTTP_303_SEE_OTHER
+        )
+    except Exception as e:
+        raise HTTPException(403, str(e))
+
+    return RedirectResponse(checkout_session.url, code=303)
 
 
 @app.route("/scan_tweets")
 async def scan_tweets(request: Request):
-    df = app.df
+    # Get Tweets
+    tweets_out = []
+    for tweet in tweepy.Paginator(app.client.get_users_tweets, id=app.user_id,
+                                  tweet_fields=['id', 'text', 'created_at'], max_results=100).flatten(limit=500):
+        tweets_out.append([tweet.id, tweet.text, tweet.created_at])
+
+    timeline_df = pd.DataFrame(tweets_out, columns=['Delete?', 'Text', 'date_full'])
+    df = timeline_df
+
     total_count = df.shape[0]
     out_df = flagDFProces(df)
     prof_df = out_df[out_df['occurance'] == 1]
 
     check_box = r"""<input type="checkbox" id="\1" name="tweet_id" value="\1">
                             <label for="\1">  </label><br>"""
-    out_table_html = str(re.sub(r'(\d{18})', check_box,
+    out_table_html = str(re.sub(r'(\d{18,19})', check_box,
                                 prof_df.drop(['date_full', 'occurance'], 1).to_html(index=False).replace('<td>',
                                                                                                          '<td align="center">')))
     p_count = prof_df.shape[0]
@@ -134,5 +175,4 @@ async def selectTweets(request: Request):
 
 if __name__ == '__main__':
     if os.environ['MODE'] == 'dev':
-        #test
         uvicorn.run(app, port=8000, host='0.0.0.0')
