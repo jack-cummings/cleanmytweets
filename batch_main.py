60889adc563b19b3482ccf56525e895e2677e1ac
jack-cummings <jmcummi2@ncsu.edu>
2022-03-28 18:25:26 -0400

    add free mode, move processing to background process

---
+++ batch_main.py
@@ -0,0 +1,63 @@
+import tweepy
+import pandas as pd
+import re
+import json
+import os
+import datetime
+from pandas import ExcelWriter
+
+def loadWords():
+    f= open("references/profane_words.json", 'r')
+    bad_words = json.load(f)
+    bad_words_pattern = ' | '.join(bad_words)
+    return bad_words_pattern, bad_words
+
+def initTwitter():
+    bearer_token = os.environ['BEARER_TOKEN']
+    # api = tweepy.API(bearer_token)
+    # # assign the values accordingly
+    # auth = tweepy.AppAuthHandler(os.environ['api_key'], os.environ['api_secret'])
+    # api = api(auth)
+    auth = tweepy.OAuth2BearerHandler(bearer_token)
+    api = tweepy.API(auth)
+    return api
+
+def scrape_tl_username(name):
+    temp_list =[]
+    for status in tweepy.Cursor(api.user_timeline, screen_name=name, tweet_mode="extended").items():
+        temp_list.append((status.full_text, status.created_at))
+    timeline_df = pd.DataFrame(temp_list, columns=['Tweet','date_full'])
+    print(f"{name}'s timeline extract complete. {timeline_df.shape[0]} Tweets")
+    return timeline_df
+
+def flagDFProces(df):
+    df['Profane Words'] = df['Tweet'].apply(lambda x: ','.join(re.findall(bad_words_pattern, x)))
+    df['occurance'] = df['Profane Words'].apply(lambda x: 1 if len(x) > 0 else 0)
+    df['Date'] = df['date_full'].apply(lambda x: datetime.datetime.date(x))
+    out_df = df[df['occurance']==1]
+    out_df = out_df[['Tweet','Profane Words', 'Date']]
+    return out_df
+
+def main(users):
+    out_dfs = {}
+    for user in users:
+        df = scrape_tl_username(user)
+        out_df = flagDFProces(df)
+        out_dfs.update({user:out_df})
+    return out_dfs
+
+def writeExcel(df_dict, outpath):
+    with ExcelWriter(outpath) as writer:
+        for user, df in df_dict.items():
+            df.to_excel(writer, user)
+
+users = ['@Dalton_ClarkNC']
+bad_words_pattern, bad_words = loadWords()
+api = initTwitter()
+dfs = main(users)
+writeExcel(dfs,'exp/user_report.xlsx')
+
+
+
+
+
--- cmn/main.py
+++ cmn/main.py
@@ -10,10 +10,8 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
 
-## Configs
 if os.environ['MODE'] == 'dev':
     import uvicorn
-
     stripe.api_key = os.environ['STRIPE_KEY_DEV']
     price = "price_1KeQ1PCsKWtKuHp0PIYQ1AnH"
 else:
@@ -21,12 +19,6 @@ else:
     # price = 'price_1KdhRoCsKWtKuHp0EfcqdUG8'
     stripe.api_key = os.environ['STRIPE_KEY_DEV']
     price = "price_1KeQ1PCsKWtKuHp0PIYQ1AnH"
-
-if os.environ['PAY_MODE'] == 'pay':
-    return_path = "create-checkout-session"
-else:
-    return_path = 'free_mode'
-
 
 def HtmlIntake(path):
     with open(path) as f:
@@ -45,7 +37,7 @@ def loadWords(mode):
 
 
 def flagDFProces(df):
-    df['Profane Words'] = df['Text'].apply(lambda x: ' , '.join(re.findall(bad_words_pattern, x)))
+    df['Profane Words'] = df['Text'].apply(lambda x: re.findall(bad_words_pattern, x))
     df['occurance'] = df['Profane Words'].apply(lambda x: 1 if len(x) > 0 else 0)
     df['Date'] = df['date_full'].apply(lambda x: datetime.datetime.date(x))
     return df
@@ -71,7 +63,6 @@ def setBasePath(mode):
 
     return basepath
 
-
 def getTweets():
     # Get Tweets
     tweets_out = []
@@ -80,10 +71,7 @@ def getTweets():
         tweets_out.append([tweet.id, tweet.text, tweet.created_at])
 
     timeline_df = pd.DataFrame(tweets_out, columns=['Delete?', 'Text', 'date_full'])
-
-    out_df = flagDFProces(timeline_df)
-    app.df = out_df
-
+    app.df = timeline_df
 
 #  initialization
 mode = os.environ['MODE']
@@ -96,50 +84,47 @@ app.auth = oauth2_handler
 templates = Jinja2Templates(directory='templates/jinja')
 
 
+
 @app.get("/")
 async def home(request: Request):
-    try:
-        authorization_url = app.auth.get_authorization_url()
-        return templates.TemplateResponse('index_j.html', {"request": request, "user_auth_link": authorization_url})
-    except:
-        return templates.TemplateResponse('error.html', {"request": request})
+    authorization_url = app.auth.get_authorization_url()
+    return templates.TemplateResponse('index_j.html', {"request": request, "user_auth_link": authorization_url})
 
 
 @app.get('/return')
 async def results(request: Request, background_tasks: BackgroundTasks):
-    try:
-        access_token = app.auth.fetch_token(str(request.url))
-        client = tweepy.Client(access_token['access_token'])
+    access_token = app.auth.fetch_token(str(request.url))
+    client = tweepy.Client(access_token['access_token'])
 
-        user = client.get_me(user_auth=False)
-        username = user.data.username
-        user_id = user.data.id
-        app.user_id = user_id
-        app.user = username
-        app.client = client
+    user = client.get_me(user_auth=False)
+    username = user.data.username
+    user_id = user.data.id
+    app.user_id = user_id
+    app.user = username
+    app.client = client
 
-        # Begin Timeline scrape
-        background_tasks.add_task(getTweets)
+    # Begin Timeline scrape
+    background_tasks.add_task(getTweets)
 
-        return templates.TemplateResponse('account_val.html', {"request": request, "user": username,
-                                                               "return_path": return_path})
-    except:
-        return templates.TemplateResponse('error.html', {"request": request})
+    return templates.TemplateResponse('account_val.html', {"request": request, "user": username})
 
 
 @app.get("/success")
 async def success(request: Request):
     return templates.TemplateResponse('payment_val.html', {"request": request, "user": app.user})
 
-@app.get("/free_mode")
-async def success(request: Request):
-    return templates.TemplateResponse('free_mode.html', {"request": request})
+
+@app.get("/cancel")
+async def cancel(request: Request):
+    return templates.TemplateResponse('cancel.html', {"request": request})
+
 
 
 @app.get('/create-checkout-session')
 async def create_checkout_session(request: Request):
+
     checkout_session = stripe.checkout.Session.create(
-        success_url=basepath + "/success?session_id={CHECKOUT_SESSION_ID}",
+        success_url=basepath+"/success?session_id={CHECKOUT_SESSION_ID}",
         cancel_url=basepath,
         payment_method_types=["card"],
         mode="payment",
@@ -153,46 +138,42 @@ async def create_checkout_session(request: Request):
 
 @app.get("/scan_tweets")
 async def scan_tweets(request: Request):
-    try:
-        out_df = app.df
-        total_count = out_df.shape[0]
-        prof_df = out_df[out_df['occurance'] == 1]
 
-        check_box = r"""<input type="checkbox" id="\1" name="tweet_id" value="\1">
-                                <label for="\1">  </label><br>"""
-        out_table_html = str(re.sub(r'(\d{18,19})', check_box,
-                                    prof_df.drop(['date_full', 'occurance'], 1).to_html(index=False).replace(
-                                        '<td>', '<td align="center">').replace(
-                                        '<tr style="text-align: right;">', '<tr style="text-align: center;">').replace(
-                                        '<table border="1" class="dataframe">', '<table class="table">')))
-        p_count = prof_df.shape[0]
+    df = app.df
 
-        return templates.TemplateResponse('returnPage_j.html', {"request": request,
-                                                                "p_count": str(p_count),
-                                                                'table': out_table_html,
-                                                                'total_count': str(total_count),
-                                                                'user': app.user})
-    except:
-        return templates.TemplateResponse('error.html', {"request": request})
+    total_count = df.shape[0]
+    out_df = flagDFProces(df)
+    prof_df = out_df[out_df['occurance'] == 1]
+
+    check_box = r"""<input type="checkbox" id="\1" name="tweet_id" value="\1">
+                            <label for="\1">  </label><br>"""
+    out_table_html = str(re.sub(r'(\d{18,19})', check_box,
+                                prof_df.drop(['date_full', 'occurance'], 1).to_html(index=False).replace(
+                                    '<td>','<td align="center">').replace(
+                                    '<tr style="text-align: right;">', '<tr style="text-align: center;">')))
+    p_count = prof_df.shape[0]
+
+    return templates.TemplateResponse('returnPage_j.html', {"request": request,
+                                                            "p_count": str(p_count),
+                                                            'table': out_table_html,
+                                                            'total_count': str(total_count),
+                                                            'user': app.user})
 
 
 @app.post('/selectTweets')
 async def selectTweets(request: Request):
-    try:
-        body = await request.body()
-        values = body.decode("utf-8").replace('tweet_id=', '').split(',')
-        if values == [""]:
-            pass
-        elif len(values) < 50:
-            for v in values:
-                app.client.delete_tweet(v, user_auth=False)
-        else:
-            return templates.TemplateResponse('over50Page.html', {'request': request})
+    body = await request.body()
+    values = body.decode("utf-8").replace('tweet_id=', '').split(',')
+    if values == [""]:
+        pass
+    elif len(values) < 50:
+        for v in values:
+            app.client.delete_tweet(v, user_auth=False)
+    else:
+        return templates.TemplateResponse('over50Page.html', {'request': request})
 
-        return templates.TemplateResponse('Tweets_deleted.html', {'request': request,
-                                                                  'count': str(len(values))})
-    except:
-        return templates.TemplateResponse('error.html', {"request": request})
+    return templates.TemplateResponse('Tweets_deleted.html', {'request': request,
+                                                              'count': str(len(values))})
 
 
 if __name__ == '__main__':
